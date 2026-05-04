7ec564449bd831b6b4050808b0a088a4b8f85466
Link4Electronics <link4electronics@gmail.com>
2026-05-04 06:55:00 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,31 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    cmake          \
+    pipewire-audio \
+    pipewire-jack  \
+    sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building stable version of SkyEmu..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/skylersaleh/SkyEmu"
+VERSION="$(curl -s https://api.github.com/repos/skylersaleh/SkyEmu/releases/latest | grep '"tag_name"' | cut -d '"' -f 4 | sed 's/^v//')"
+git clone --depth 1 "$REPO" ./SkyEmu
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+cd ./SkyEmu
+git checkout "$VERSION"
+cmake . \
+    -G 'Unix Makefiles' \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D USE_SYSTEM_CURL=ON \
+    -D USE_SYSTEM_OPENSSL=ON \
+    -D USE_SYSTEM_SDL2=ON
+make -j$(nproc)
+mv -v bin/SkyEmu ../AppDir/bin
