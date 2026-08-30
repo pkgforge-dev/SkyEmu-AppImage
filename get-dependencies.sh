6bfde9d11278f9a6ef1344ab0fb95f4815f638b0
Link4Electronics <link4electronics@gmail.com>
2026-08-30 08:39:32 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,15 +7,17 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+    cmake          \
     pipewire-audio \
-    pipewire-jack
+    pipewire-jack  \
+    sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 # Comment this out if you need an AUR package
-make-aur-package skyemu
+#make-aur-package skyemu
 
 # If the application needs to be manually built that has to be done down here
 
