e488db447c5e896d7c82d679b5788d625e0e62f0
Link4Electronics <link4electronics@gmail.com>
2026-08-30 08:58:17 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,8 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q skyemu | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
@@ -14,7 +13,7 @@ export STARTUPWMCLASS=
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/SkyEmu
+quick-sharun ./AppDir/bin/SkyEmu
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
