dd1562078515eb22d0957f051f88276b3bac7cd2
Link4Electronics <link4electronics@gmail.com>
2026-08-30 09:00:10 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,8 +8,6 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/skylersaleh/SkyEmu/refs/heads/dev/src/resources/icons/icon-nobg.png
-export DESKTOP=/usr/share/applications/skyemu.desktop
-export STARTUPWMCLASS=
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
